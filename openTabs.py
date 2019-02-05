2f8219418995844e0c9cc69544ff43a9cce20c7f
Furishon <goransson.erik2001@gmail.com>
2019-02-05 10:40:20 +0100

    Added url reading from file

--- openTabs.py
+++ openTabs.py
@@ -1,14 +1,21 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
-
 driver = webdriver.Chrome()
 
-print('OPENING https://xkcd.com...')
-driver.get('https://xkcd.com')  
-time.sleep(3)
-print('OPENING https://aeon.co...')
-driver.get('https://aeon.co')  
-time.sleep(3)
+def openUrl(url, onTime):
+	'''OPENS A SPECIFIED URL AND KEEPS IT OPEN FOR (ATLEAST) A SPECIFIED AMOUNT OF TIME
+ARGS:
+	url: string, THE EXACT URL TO OPEN,
+	onTime: int, THE NUMBER OF SECONDS TO KEEP URL OPEN'''
+	print('OPENING {}'.format(url))
+	driver.get(url)
+	time.sleep(onTime) 
+
+f = open('urls.txt', 'r')
+urlList = f.readlines()
+for url in urlList:
+	openUrl(url=str(url), onTime=3)	
+
 print('CLOSING...') 
 driver.close()
